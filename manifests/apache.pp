0b7728c25fbb6e841d4ed947a9e15637892524d0
Thomas <detemmerman.thomas@hotmail.com>
2017-03-04 17:00:02 +0100

    tempalte

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -6,10 +6,15 @@ node default {
 		ensure => running,
 		enable => true,
 		}
-	
+	#file { '/var/www/html/index.html':
+  #		ensure  => file,
+  #		content => template('apache/index.html.erb'),
+  # Loads /etc/puppetlabs/code/environments/production/modules/ntp/templates/ntp.conf.erb
+#}
+
 	# firewall { '200 allow http':
 	 #    dport    => '80',
 	  #  proto    => 'tcp',
 	   #  action   => 'accept',
 	#	 }
-} 
+}
